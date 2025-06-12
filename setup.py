8d254c4d23da29abd412408ee9ef75ab3148d3b5
InduwaraGayashan001 <141438053+InduwaraGayashan001@users.noreply.github.com>
2025-06-13 00:00:25 +0530

    Add setup.py for package configuration

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='generative-ai-llm',
+    version='0.0.0',
+    author='Induara Gayashan',
+    author_email='induwaragayashan@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+)
